146ddad5344759297e6c66e8f3f36fc3a649194b
mukhriza tunnisa <ica@192.168.1.7>
2023-08-12 20:16:21 +0700

    first commit

---
+++ hw-10/controller/movie.controller.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const router = express.Router()
+const db = require('./../models')
+const Movie = db.Movie
+const authenticateTokenMiddleware = require('../middleware/authentication')
+const cloudinaryConfig = require('../config/cloudinary')
+const movieService = require('../service/movie.service')
+
+// Inject middleware as global middleware
+// router.use(authenticateTokenMiddleware)
+
+// GET /movies
+router.get('/api/movies', async (request, response) => {
+  const movies = await movieService.findAllMovies(request.query.page, request.query.size)
+  const movieCount = await movieService.countMovies()
+
+  return response.status(200).json({
+    data: movies,
+    meta: {
+      page: request.query.page,
+      count: movieCount,
+      size: movies.length,
+    }
+  })
+})
+
+// GET /movies/:id
+router.get('/api/movies/:id', async (request, response) => {
+  const movie = await movieService.findByMovieId(request.params.id);
+
+  if(!movie) return response.status(404).json({message: 'Movies not found'})
+
+  return response.status(200).json({data: movie})
+})
+
+// POST /movies
+router.post('/api/movies', async (request, response) => {
+  // Upload movie photo to cloudinary
+  const uploadedFile = await cloudinaryConfig.uploader.upload(request.files.photo.path)
+
+  const movie = await Movie.create({
+    title: request.fields.title,
+    genre: request.fields.genre,
+    year: request.fields.year,
+    photo: uploadedFile?.secure_url
+  })
+
+  if(!movie) return response.status(422).json({message: 'Failed create movie. Please try again'})
+
+  return response.status(201).json({ data: movie })
+})
+
+// PUT /movies/:id
+router.put('/api/movies/:id', async (request, response) => {
+  const movie = await Movie.findByPk(request.params.id)
+
+  if(!movie) return response.status(404).json({message: 'Movies not found'})
+
+  Movie.update(request.body, { where: {id: request.params.id }})
+
+  return response.status(200).json({ message: 'Movie updated'})
+})
+
+// DELETE /movies/:id
+router.delete('/api/movies/:id', async (request, response) => {
+  const movie = await Movie.findByPk(request.params.id)
+
+  if(!movie) return response.status(404).json({message: 'Movies not found'})
+
+  Movie.destroy({where: { id: request.params.id }})
+
+  return response.status(200).json({ data: movie })
+})
+
+module.exports = router
---
+++ hw-10/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const formidableMiddleware = require('express-formidable');
+const PORT = 8081 || process.env.PORT;
+const userController = require('./controller/user.controller')
+const movieController = require('./controller/movie.controller')
+const authenticationController = require('./controller/authentication.controller')
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+app.use(formidableMiddleware())
+app.use(morgan('combined'))
+// app.use(authenticationController)
+app.use(userController)
+app.use(movieController)
+
+
+app.get('/ping', (req, res) => {
+  res.json({ping: true})
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
